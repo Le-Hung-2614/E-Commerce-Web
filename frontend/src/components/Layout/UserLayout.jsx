2691523a4a618ce2db2bbfd1c95eb999dd45a506
Le-Hung-2614 <BlueBim123@gmail.com>
2025-09-06 15:04:05 +0700

    init project

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,16 @@
+import Footer from "../Common/Footer";
+import Header from "../Common/Header";
+
+const UserLayout = () => {
+  return (
+<>
+  {/* Header */}
+  <Header />
+  {/* Main Content */}
+  {/* Footer */}
+  <Footer />
+</>
+  );
+};
+
+export default UserLayout
